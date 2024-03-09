e65cd618accf8480f5feb54a3fc5c1be5b65f424
Leandro Gedeon <gedeonleandro@gmail.com>
2024-03-09 19:35:51 -0300

    updates

--- config/dev.config.js
+++ config/dev.config.js
@@ -1,20 +1,20 @@
 "use strict";
 
-let self 	  = {};
-var base_url  = process.env.APP_BASE_URL || 'http://localhost:5005';
+let self = {};
+var base_url = process.env.APP_BASE_URL || 'http://localhost:5005';
 
 self.config = {
 	metas: {
-		title : "dosmil12.com.ar",
-		color : "#000000",
-		url_site: base_url+"/",
-		og_image: base_url+"/statics/images/dosmil12comar.jpg",
-		canonical : "http://www.dosmil12.com.ar/"
+		title: "somosvos.org",
+		color: "#000000",
+		url_site: base_url + "/",
+		og_image: base_url + "/statics/images/TBD.jpg",
+		canonical: "http://www.somosvos.org/"
 	},
-	sitePathCom : base_url,
-	sitePathComAr : base_url,
-	sitePath : base_url,
-	sourcePath : base_url+"/statics",
+	sitePathCom: base_url,
+	sitePathComAr: base_url,
+	sitePath: base_url,
+	sourcePath: base_url + "/statics",
 	phoneChat: "https://wa.me/5491158888423",
 	portfolio: "https://www.behance.net/leandrogedeon",
 };
