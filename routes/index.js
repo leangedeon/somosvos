40ebaabc1d658f33f38737ae92c9966ea2059589
Leandro Gedeon <gedeonleandro@gmail.com>
2024-03-09 19:59:40 -0300

    first content

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
 "use strict"
 
-const express  = require('express');
-const router   = express.Router();
+const express = require('express');
+const router = express.Router();
 const uaparser = require('ua-parser-js');
-const main     = require('../controllers/MainController');
+const main = require('../controllers/MainController');
 
 
 router.all('*', function (req, res, next) {
 
     let app = req.app,
-        ua  = new uaparser(req.get('User-Agent'));
+        ua = new uaparser(req.get('User-Agent'));
 
-    if(ua.getDevice().type === "mobile" || ua.getDevice().type === "wearable" || ua.getDevice().type === "embedded"){
-    	res.locals.device = "mobile";	
-    }else{
-    	res.locals.device = "desktop";
+    if (ua.getDevice().type === "mobile" || ua.getDevice().type === "wearable" || ua.getDevice().type === "embedded") {
+        res.locals.device = "mobile";
+    } else {
+        res.locals.device = "desktop";
     }
     console.log("Device => ", res.locals.device);
     next();
@@ -32,3 +32,17 @@ router.get('/sitemap.xml', (req, res, next) => main.sitemap(req, res, next));
 
 
 module.exports = router;
+
+
+// somosvos.org
+// rectangular-lobster-w0dgsrsb0df1rt8a7gnbtan3.herokudns.com
+
+// www.somosvos.org
+// secret-island-oyv2k50p481cb40jp7baewop.herokudns.com
+
+// www.somosvos.com
+// round-plum-e4wyujnfxwfotm3vlf8lv9ge.herokudns.com
+
+
+// somosvos.com
+// computational-chicken-gaai1ewld4kqllx0ptfo3cgp.herokudns.com
